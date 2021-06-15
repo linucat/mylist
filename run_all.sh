53e7faa9f5beaf7b4cc6e0947ae0d80ad6e8ceb2
nakadollar <nakadollar@gmail.com>
2021-06-15 19:41:32 +0700

    new

---
+++ run_all.sh
@@ -0,0 +1,19 @@
+#To the folder
+cd /home/lkthedev/Desktop/Mygit/coinmasterwiki.com
+
+#Pull to update
+git pull
+echo "Pull done..."
+
+#Run main.py from env python path
+env/bin/python3 main.py
+
+#Then do git jobs
+git add .
+echo 'Add done...'
+
+git commit -m new
+echo 'Commit done...'
+
+git push
+
